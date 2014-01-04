80ad04a8719fadcd834357a3879be34f91200f74
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-01-04 04:44:47 +0000

    surfaceflinger: Consolidate display orientation compensation hooks
    Dedupe rotation calculation code and make rotation a statically
    available property in the DisplayDevice
    Change-Id: Ic517ab0d2c05026cd6fa46d664aab7926be17b62
    
    Conflicts:
    	services/surfaceflinger/DisplayDevice.cpp
    	services/surfaceflinger/SurfaceFlinger.cpp

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -747,10 +747,26 @@ status_t SurfaceFlinger::getDisplayInfo(const sp<IBinder>& display, DisplayInfo*
         info->orientation = 0;
     }
 
+<<<<<<< HEAD
     info->w = hwc.getWidth(type);
     info->h = hwc.getHeight(type);
     info->xdpi = xdpi;
     info->ydpi = ydpi;
+=======
+    int additionalRot = mDisplays[0]->getHardwareOrientation() / 90;
+    if ((type == DisplayDevice::DISPLAY_PRIMARY) && (additionalRot & DisplayState::eOrientationSwapMask)) {
+        info->h = hwc.getWidth(type);
+        info->w = hwc.getHeight(type);
+        info->xdpi = ydpi;
+        info->ydpi = xdpi;
+    }
+    else {
+        info->w = hwc.getWidth(type);
+        info->h = hwc.getHeight(type);
+        info->xdpi = xdpi;
+        info->ydpi = ydpi;
+    }
+>>>>>>> 2f0caf7... surfaceflinger: Consolidate display orientation compensation hooks
     info->fps = float(1e9 / hwc.getRefreshPeriod(type));
 
     // All non-virtual displays are currently considered secure.
